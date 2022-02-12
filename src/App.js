a982fffda8d7fcb0292a054fa33a6a5f8b543450
mukulbindal <mukulbindal170299@gmail.com>
2022-02-12 20:09:57 +0530

    Added Menu

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {Component} from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-
+import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            
           </div>
         </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
